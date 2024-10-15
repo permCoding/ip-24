92266eba6f3caa3f505dc01b7a4316cc2cebf1fe
Андрей Беляков <ttxiom@gmail.com>
2024-10-15 12:30:30 +0500

    up

---
+++ lections/lect-06 sort/01.py
@@ -0,0 +1,20 @@
+"""
+- линейный поиск - N
+- бинарный поиск - log2(N)
+- N*N
+- N*log2(N)
+"""
+
+from random import randint as rnd
+
+# 1_000 $
+cmp = rnd(1, 1000)
+
+while True:
+    input(' ebc 98y wue eu - ')
+    
+    hmn = 239847290
+    print('> <')
+    # - 100 $
+
+print('сколько шагов или сколько осталось денег')
