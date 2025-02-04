43aba7a545a7a0be9f99f91bfbf956281ea60ca6
Андрей Беляков <ttxiom@gmail.com>
2025-02-04 14:47:58 +0500

    up

--- part_2/lections/lect-01-request/01-index-tiobe/01.py
+++ part_2/lections/lect-01-request/01-index-tiobe/01.py
@@ -5,12 +5,12 @@ def get_lines_from_url(url):
     """получить список строк из файла по адресу"""
     req = requests.get(url)
     req.encoding = "utf8"
-    return req.text.split('\n')
+    return req.text
 
 
-url = "https://pcoding.ru/csv/exam03.txt"
-url = "https://pcoding.ru/csv/exam_balls.csv"
-url = "https://pcoding.ru/csv/14.txt"
-url = "https://www.tiobe.com/tiobe-index/"
-lines = get_lines_from_url(url)
-print(lines)
+url = "https://pcoding.ru/csv/exam02.html"
+# url = "https://www.tiobe.com/tiobe-index/"
+
+txt = get_lines_from_url(url)
+with open('./02.html', mode='w', encoding='utf8') as f:
+    f.write(txt)
--- part_2/lections/lect-01-request/01-index-tiobe/02.py
+++ part_2/lections/lect-01-request/01-index-tiobe/02.py
@@ -2,28 +2,20 @@ import requests  # pip install requests
 
 
 def get_from_url(url):
-    """получить список строк из файла по адресу"""
     req = requests.get(url)
     req.encoding = "utf8"
     return req.text
 
+def get_from_file():
+    pass
+    pass
 
-url = "https://www.tiobe.com/tiobe-index/"
-html = get_from_url(url)
-pos = html.find('<table id="top20"')
-posStart = html.find('<tbody>', pos) + 8
-posFinish = html.find('</tbody>', pos)
-tbody = html[posStart: posFinish]
 
-print(tbody)
-"""
-написать программу
-которая сформирует csv-файл с данными
-разместив их в три колонки:
-место_за_2024_год;название_языка_программирования;рейтинг
+url = "https://pcoding.ru/csv/exam02.html"
 
-Пример:
-1;Python;22.85%
-2;C++;10.64%
-...
-"""
+txt = get_from_url(url)
+
+posA = txt.find('<tr class="person">')
+posB = txt.find('</tr>', posA)
+tagTr = txt[posA:posB+5]
+print(tagTr)
